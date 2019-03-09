9fa6920b5f5ca6a5e495928c3fc793c7c1d16740
Bill Jackson <billjackson@cakewalk.com>
2019-03-09 17:02:13 -0500

    Simplified movement script

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -11,13 +11,12 @@ public class BlockMovement : MonoBehaviour
     [Range(.1f, 1f)]
     private float speed = .25f;
     [SerializeField]
-    AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    private AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
     private bool locked;
     private Transform originParent;
     private Transform rotator;
-    private Queue<UnityAction> queuedMoves = new Queue<UnityAction>();
-    private LayerMask mask;
+    private Queue<System.Action> queuedMoves = new Queue<System.Action>();
 
     void Start()
     {
@@ -25,7 +24,6 @@ public class BlockMovement : MonoBehaviour
         {
             playerCube = GetComponentInChildren<BoxCollider>();
         }
-        mask = LayerMask.GetMask(LayerMask.LayerToName(playerCube.gameObject.layer));
         rotator = new GameObject().transform;
         rotator.parent = transform;
         rotator.name = "_rotator";
@@ -33,121 +31,53 @@ public class BlockMovement : MonoBehaviour
 
     void Update()
     {
+        // Add movement to the queue
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            queuedMoves.Enqueue(Forward);
+            queuedMoves.Enqueue(() => PivotForMove(Vector3.forward));
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            queuedMoves.Enqueue(Back);
+            queuedMoves.Enqueue(() => PivotForMove(Vector3.back));
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            queuedMoves.Enqueue(Right);
+            queuedMoves.Enqueue(() => PivotForMove(Vector3.right));
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            queuedMoves.Enqueue(Left);
+            queuedMoves.Enqueue(() => PivotForMove(Vector3.left));
         }
 
+        // If there's a movement to make, and one's not currently running, make the move
         if (!locked && queuedMoves.Count > 0)
         {
             queuedMoves.Dequeue().Invoke();
         }
     }
-
-    private void Forward()
+    private void PivotForMove(Vector3 direction)
     {
+        // ensure the object isn't alread moving
         if (locked)
             return;
 
+        // set to locked state to prevent other moves from running until we're done
         locked = true;
+        // Remember our original parent
         originParent = transform.parent;
-        Vector3 pivot = Vector3.zero;
-        RaycastHit hitInfo;
-        if (Physics.Raycast(playerCube.transform.position + Vector3.forward * 10, Vector3.back, out hitInfo, 10f, mask))
-        {
-            pivot.z = hitInfo.point.z;
-        }
-        if (Physics.Raycast(playerCube.transform.position + Vector3.down * 10, Vector3.up, out hitInfo, 10f, mask))
-        {
-            pivot.y = hitInfo.point.y;
-        }
+        // Get the pivot: the farthest point at the bottom toward the direction requested. 
+        Vector3 pivot = playerCube.ClosestPointOnBounds(transform.position + direction.normalized * 10f + Vector3.down * 10f) ;
+
+        // Set rotator transform to that position
         rotator.parent = null;
         rotator.position = pivot;
+        // Parent self to rotator
         transform.parent = rotator;
-        StartCoroutine(Move(new Vector3(90f, 0f, 0f)));
+        // Start movement coroutine
+        StartCoroutine(Move(rotator.eulerAngles + Vector3.Cross(direction, Vector3.down) * 90f));
     }
 
-    private void Back()
-    {
-        if (locked)
-            return;
-
-        locked = true;
-        originParent = transform.parent;
-        Vector3 pivot = Vector3.zero;
-        RaycastHit hitInfo;
-        if (Physics.Raycast(playerCube.transform.position + Vector3.back * 2, Vector3.forward, out hitInfo, 10f, mask))
-        {
-            pivot.z = hitInfo.point.z;
-        }
-        if (Physics.Raycast(playerCube.transform.position + Vector3.down * 2, Vector3.up, out hitInfo, 10f, mask))
-        {
-            pivot.y = hitInfo.point.y;
-        }
-        rotator.parent = null;
-        rotator.position = pivot;
-        transform.parent = rotator;
-        StartCoroutine(Move(new Vector3(-90f, 0f, 0f)));
-    }
-
-    private void Right()
-    {
-        if (locked)
-            return;
-
-        locked = true;
-        originParent = transform.parent;
-        Vector3 pivot = Vector3.zero;
-        RaycastHit hitInfo;
-        if (Physics.Raycast(playerCube.transform.position + Vector3.right * 2, Vector3.left, out hitInfo, 10f, mask))
-        {
-            pivot.x = hitInfo.point.x;
-        }
-        if (Physics.Raycast(playerCube.transform.position + Vector3.down * 2, Vector3.up, out hitInfo, 10f, mask))
-        {
-            pivot.y = hitInfo.point.y;
-        }
-        rotator.parent = null;
-        rotator.position = pivot;
-        transform.parent = rotator;
-        StartCoroutine(Move(new Vector3(0f, 0f, -90f)));
-    }
-
-    private void Left()
-    {
-        if (locked)
-            return;
-
-        locked = true;
-        originParent = transform.parent;
-        Vector3 pivot = Vector3.zero;
-        RaycastHit hitInfo;
-        if (Physics.Raycast(playerCube.transform.position + Vector3.left * 2, Vector3.right, out hitInfo, 10f, mask))
-        {
-            pivot.x = hitInfo.point.x;
-        }
-        if (Physics.Raycast(playerCube.transform.position + Vector3.down * 2, Vector3.up, out hitInfo, 10f, mask))
-        {
-            pivot.y = hitInfo.point.y;
-        }
-        rotator.parent = null;
-        rotator.position = pivot;
-        transform.parent = rotator;
-        StartCoroutine(Move(new Vector3(0f, 0f, 90f)));
-    }
-
+    // Rotate rotator object by the requested duration over "speed" time
     private IEnumerator Move(Vector3 rotation)
     {
         if (!locked)
